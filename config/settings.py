ecd8c9357b86a160d6df45f2e41db535c249c71c
lisabeth23 <amevivielisabeth6@gmail.com>
2026-07-24 11:04:19 +0000

    Ajout de localhost:5173 (Vite) dans CORS_ALLOWED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # le frontend React d'Elisabeth que Eli va créer
+      "http://localhost:5173",
 ]
 
 REST_FRAMEWORK = {
